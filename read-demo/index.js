cb0fdaebbb6841baadbdcb66e07c9d20a00c2d83
krhougs <os@kt.je>
2023-11-08 03:27:31 +0800

    update demo

--- read-demo/index.js
+++ read-demo/index.js
@@ -1,10 +1,10 @@
-import { Client } from 'https://deno.land/x/mqtt@0.1.2/deno/mod.ts'
+import {Client} from 'https://deno.land/x/mqtt@0.1.2/deno/mod.ts'
+import dephyProto from "npm:dephy-proto"
 
-import * as RawMessage from "./proto/messages/dephy/message/RawMessage.ts"
-import * as SignedMessage from "./proto/messages/dephy/message/SignedMessage.ts"
+const {RawMessage, SignedMessage} = dephyProto
 
 const url = Deno.env.get("BROKER_URL") || 'mqtt://demo-edge.dephy.io:1883'
-const client = new Client({ url })
+const client = new Client({url})
 
 await client.connect()
 console.log("Connected to broker.")
@@ -12,11 +12,11 @@ console.log("Connected to broker.")
 await client.subscribe("/dephy/signed_message");
 
 client.on('message', (_, payload) => {
-  try {
-    const msg = SignedMessage.decodeBinary(payload);
-    const raw = RawMessage.decodeBinary(msg.raw);
-    console.log(raw)
-  } catch (error) {
-    console.log({ payload, error })
-  }
+    try {
+        const msg = SignedMessage.decodeBinary(payload);
+        const raw = RawMessage.decodeBinary(msg.raw);
+        console.log(raw)
+    } catch (error) {
+        console.log({payload, error})
+    }
 });
